2494f4979da5f62008952212719d0bc1b94f567e
Jason Paige <j@jasonpaige.co.uk>
2023-03-17 22:37:08 +0000

    fix

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,29 +1,21 @@
 import Script from "next/script";
 import { memo, useEffect } from "react";
-const TRACKING_ID = "G-WB27FGH6QJ";
 const GoogleAnalytics = () => {
-  useEffect(() => {
-    gtag("event", "page_view", {
-      page_path: window.location.pathname,
-      send_to: TRACKING_ID,
-    });
-  }, []);
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
-      ></Script>
-      {/* 👇 gtag function definition. notice that we don't send page views at this point.  */}
-      <Script
-        id="gtag-init"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `,
-        }}
-      />
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WB27FGH6QJ');
+        `}
+    </Script>
     </>
   );
 };
