ce0f72c0cd2a47edf24f4e0612f6c3c62020ea67
Jason Paige <j@jasonpaige.co.uk>
2023-03-17 19:34:42 +0000

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,14 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WB27FGH6QJ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WB27FGH6QJ');
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
