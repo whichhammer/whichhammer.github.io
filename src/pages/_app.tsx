e539b8e193de1461edcb8b4e31cacf0817890133
Jason Paige <j@jasonpaige.co.uk>
2023-03-20 13:03:28 +0000

    fix for GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import Head from "next/head";
 import type { AppProps } from 'next/app'
-import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 
 type WindowWithDataLayer = Window & {
   dataLayer: Record<string, any>[];
@@ -16,8 +16,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <GoogleAnalytics />
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-WB27FGH6QJ`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WB27FGH6QJ', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,19 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        {/* Necessary to prevent error: window.gtag is not defined for Next.js-hydration */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            `,
-          }}
-        />
+      <Main />
+      <NextScript />
       </body>
     </Html>
   )
